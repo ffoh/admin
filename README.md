e7bc4b5578b7886b7b275055258af49b970eb6f7
Steffen Moeller <steffen_moeller@gmx.de>
2014-07-02 00:52:39 +0200

    Allowing selected sites to circumvent anonymisation

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ admin
 Tools for configuring Freifunk Network in Ostholstein, Schleswig-Holstein, Germany
 
  * iptables.sh      - automated setup of Freifunk compatible firewall
+ * direct_route.sh  - rudimentary white list
+ 
---
+++ direct_route.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+gateway=$(LANG=C ifconfig eth0 | grep "inet addr" |cut -f2 -d:|cut -f1 -d\ )
+echo "Identified gateway as '$gateway'"
+if [ -z "$gateway" ]; then
+	echo "E: Could not identify gateway via ifconfig eth0"
+	exit 1
+fi
+
+function ipdirect () {
+	ip=$1
+	if ! ip route list table freifunk | grep -q $ip; then
+		echo "I: Adding route for $ip via $gateway for table freifunk"
+		ip route add $ip via 141.101.36.67 table freifunk
+	else
+		echo "I: Route for $ip is existing - skipped"
+	fi
+}
+
+IPs=$(cat <<EOIPS 
+google-public-dns-a.google.com
+www.google.com
+google.com
+spiegel.de
+www.spiegel.de
+mail.google.com
+gmail.com
+spotify.com
+www.spotify.com
+arte.tv
+www.arte.tv
+tagesschau.de
+www.tagesschau.de
+ndr.de
+www.ndr.de
+zdf.de
+www.zdf.de
+spotify.com
+www.spotify.com
+ftp.de.debian.org
+zatoo.com
+www.zatoo.com
+amazon.de
+www.amazon.de
+EOIPS
+)
+
+for n in $IPs
+do
+	for IP in $(host $n |grep "has address" | cut -f4 -d\ )
+	do
+		#echo "$n : $IP"
+		ipdirect $IP
+	done
+done
