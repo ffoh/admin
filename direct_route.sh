5c3428618c020778046cfd34b7fe646845a8012a
Freifunk Ostholstein e.V <info@ostholstein.freifunk.net>
2014-07-07 00:46:23 +0200

    Phoenix und NDR funktinoieren white listed

--- direct_route.sh
+++ direct_route.sh
@@ -38,19 +38,33 @@ function ipindirect () {
 	fi
 }
 
-IPs=$(cat <<EOIPS 
+IPs=$(cat <<EOIPS | grep -v ^#
+#
+ostholstein.freifunk.net
+gw1.ostholstein.freifunk.net
+gw2.ostholstein.freifunk.net
+#
 google-public-dns-a.google.com
 www.google.com
 google.com
 google.de
 www.google.de
 apis.google.de
-spiegel.de
-www.spiegel.de
+www.google-analytics.com
 mail.google.com
 gmail.com
+spiegel.de
+www.spiegel.de
+cdn1.spiegel.de
+cdn2.spiegel.de
+cdn3.spiegel.de
+cdn4.spiegel.de
+magazin.spiegel.de
+spiegel.ivwbox.de
+c.spiegel.de
 spotify.com
 www.spotify.com
+# Arte - begin
 arte.tv
 info.arte.tv
 future.arte.tv
@@ -58,15 +72,81 @@ creative.arte.tv
 concert.arte.tv
 cinema.arte.tv
 www.arte.tv
+reports.cedexis.com
+org-www.arte.tv
+probes.cedexis.com
+probe.cedexis.org
+eu-ems1.joyent.bench.cedexis.com
+www.googleapis.com
+client1.google.com
+radar.cedexis.com
+llnwop-eu.cedexis.com
+llnwop.cedexis.com
+cedexis.cdn.mediactive-network.net
+sjc2.voxcloud.cedexis.com
+dca.sl.bench.cedexis.com
+ecadn-eu.cedexis.com
+logi104.xiti.com
+videos.arte.tv
+fonts.googleapis.com
+connect.facebook.net
+www.googletagmanager.com
+graph.facebook.com
+www-secure.arte.tv
+limelight.cedexis.com
+wac.799d.i1.cdndelivery.com
+artestras.vo.llnwd.net
+p.jwpcdn.com
+level3.cedexis.com
+logs1136.xiti.com
+fastlydsa.bench.cedexis.com
+playout.3qsdn.com
+c3de.wpc.azureedge.net
+ak.c.ooyala.com
+az315059.vo.msecnd.net
+cdn.api.twitter.com
+cedexis.gccdn.net
+cs600.wac.edgecastcdn.net
+gb2.cedexis.swiftserve.com
+hcacheg.tdf-cdn.com
+logc136.xiti.com
+anycast.cedexis.com
+cedexis-test01.insnw.net
+cedexis.a.cdnify.io
+cloudfront-dsa.cedexis.com
+i1-js-14-3-01-10106-780860262-i.init.cedexis-radar.net
+sita-atl.bench.cedexis.com
+cedexis2.cachefly.net
+cds.z5t8n6p8.hwcdn.net
+a.disquscdn.com
+b.scorecardresearch.com
+go.disqus.com
+cloud.typography.com
+# Arte - end
+ebay.de
+www.ebay.de
+# RBB - start
+www.rbb-online.de
+rbb-online.de
+mediathek.rbb-online.de
+rbb.ivwbox.de
+rbb.ic.llnwd.net
+# RBB - end
 tagesschau.de
 www.tagesschau.de
 ndr.de
+ndr.ivwbox.de
+ndr_fs-lh.akamaidhd.net
 www.ndr.de
-zdf.de
-www.zdf.de
+www.eurovision.de
+players.edgesuite.net
+hds.ndr.de
+de.ioam.de
 spotify.com
 www.spotify.com
 ftp.de.debian.org
+anonscm.debian.org
+alioth.debian.org
 zatoo.com
 www.zatoo.com
 amazon.de
@@ -91,17 +171,32 @@ www.gmx.net
 navigator.gmx.net
 github.com
 www.github.com
-ostholstein.freifunk.net
-gw1.ostholstein.freifunk.net
-gw2.ostholstein.freifunk.net
 freshmeat.net
 bronline.de
-
+bralpha.de
+br.de
+bronline.ivwbox.de
+cdn-vod-hds.br.de
+www.edgesuite.net
+79423.analytics.edgesuite.net
+mal140-r.analytics.edgesuite.net
+de.sitestat.com
+#
+phoenix.de
+www.phoenix.de
+webcam.zdf.de
+phoenix.ivwbox.de
+fstreaming.zdf.de
+secure-eu.imrworldwide.com
+zdf_hds_de-f.akamaihd.net
+zdf.de
+www.zdf.de
 EOIPS
 )
 
 for n in $IPs
 do
+	echo "$n"
 	if false; then
 		echo "I: Removing $IP direct link"
 		if false; then
@@ -118,7 +213,6 @@ do
 	else
 		for IP in $(host $n |grep "has address" | cut -f4 -d\ )
 		do
-			#echo "$n : $IP"
 			ipdirect $IP
 		done
 	fi
