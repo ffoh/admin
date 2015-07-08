7a1e64a2c709d438b56335e989e5ccd31dca8605
Steffen Möller <steffen_moeller@gmx.de>
2015-07-09 01:48:41 +0200

    Also preparing for complete disappearance of mullvad

--- cron/replace_default_route.sh
+++ cron/replace_default_route.sh
@@ -1,12 +1,18 @@
 #!/bin/dash
 
-defaultrouteno=$(ip route list table freifunk |grep default | wc -l)
+if ifconfig | grep -q mullvad; then
+	#echo ok
+	defaultrouteno=$(ip route list table freifunk |grep default | wc -l)
+else
+	/etc/init.d/openvpn restart
+	defaultrouteno=0
+fi
 #echo "I: defaultrouteno: $defaultrouteno"
 if [ 0 -eq $defaultrouteno ];  then
 	mullvadip=$(LANG=C ifconfig mullvad | head -n 2 |grep inet|tr " " "\n" | grep addr | cut -f2 -d:)
 	if [ "x$mullvadip" != "x" ]; then
-		echo ip route replace default via $mullvadip table freifunk
-		echo "I: Mullvad IP: $mullvadip"
+		ip route replace default via $mullvadip table freifunk
+		echo "I: Updated default route to mullvad IP: $mullvadip"
 	else
 		echo "E: could not determine mullvadip"
 	fi
