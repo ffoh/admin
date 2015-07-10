15b4391dddf78c508c09f105ae5fed9c56fabe1f
Steffen Möller <steffen_moeller@gmx.de>
2015-07-10 10:22:44 +0200

    Introducing 'tee' to replace_default_route

--- cron/replace_default_route.sh
+++ cron/replace_default_route.sh
@@ -4,26 +4,24 @@ LOGFILE=/var/log/opensshRestart.log
 
 
 
-if [ -f /var/run/openvpn.mullvad.pid ] && ifconfig mullvad | grep -q inet; then
+if [ -f /var/run/openvpn.mullvad.pid ] && /sbin/ifconfig mullvad | grep -q inet; then
 	echo ok
 	defaultrouteno=$(ip route list table freifunk |grep default | wc -l)
 else
-	date >> $LOGFILE
-	echo "* Restart" >> $LOGFILE
-	/etc/init.d/openvpn restart >> $LOGFILE
+	date | tee -a $LOGFILE
+	echo "* Restart" | tee -a $LOGFILE
+	/etc/init.d/openvpn restart | tee -a $LOGFILE
 	sleep 10
 	defaultrouteno=0
 fi
 #echo "I: defaultrouteno: $defaultrouteno"
 if [ 0 -eq $defaultrouteno ];  then
-	mullvadip=$(LANG=C ifconfig mullvad | head -n 2 |grep inet|tr " " "\n" | grep addr | cut -f2 -d:)
-	date >> $LOGFILE
+	mullvadip=$(LANG=C /sbin/ifconfig mullvad | head -n 2 |grep inet|tr " " "\n" | grep addr | cut -f2 -d:)
+	date | tee -a $LOGFILE
 	if [ "x$mullvadip" != "x" ]; then
-		ip route replace default via $mullvadip table freifunk
-		echo "I: Updated default route to mullvad IP: $mullvadip"
-		echo "* Update to $mullvadip" >> $LOGFILE
+		ip route replace default via $mullvadip table freifunk | tee -a $LOGFILE
+		echo "* I: Update to $mullvadip" | tee -a $LOGFILE
 	else
-		echo "E: could not determine mullvadip"
-		echo "* could not determine mullvadip" >> $LOGFILE
+		echo "* E: Could not determine mullvadip" | tee -a $LOGFILE
 	fi
 fi
