9a092d662f1ec70e7f8c94cc139df31fd4092922
Steffen Moeller <steffen_moeller@gmx.de>
2016-10-22 17:20:49 +0200

    First release candidate of filter_nodes.sh

--- filter_nodes.sh
+++ filter_nodes.sh
@@ -28,26 +28,33 @@ if [ ! -r ${PREFIX}tracelist ]; then
 	do
 		#echo $i
 		echo -n "$i " && echo $(( $(batctl tr $i|wc -l) -1 ))
-	done > ${PREFIX}tracelist
+	done | grep ":" > ${PREFIX}01_tracelist
 fi
 
-grep '2$' ${PREFIX}tracelist | cut -f1 -d\  | sort > ${PREFIX}candidates
-grep '3$' ${PREFIX}tracelist | cut -f1 -d\  | sort > ${PREFIX}meshing
+grep '2$' ${PREFIX}01_tracelist | cut -f1 -d\  | sort > ${PREFIX}02_candidates
+grep '3$' ${PREFIX}01_tracelist | cut -f1 -d\  | sort > ${PREFIX}02_meshing
 
-cat ${PREFIX}meshing | while read macaddress
+cat ${PREFIX}02_meshing | while read macaddress
 do
 	batctl tr $macaddress | awk '{print $2}' 
-done | sort > ${PREFIX}neededForMeshing
+done | sort > ${PREFIX}03_neededForMeshing
 
-join --check-order -v 1  ${PREFIX}candidates  ${PREFIX}neededForMeshing > ${PREFIX}permitted
+join --check-order -v 1  ${PREFIX}02_candidates  ${PREFIX}03_neededForMeshing > ${PREFIX}04_permitted
 
-
-cat ${PREFIX}permitted | while read macaddress
+echo "#!/bin/bash -e" > ${PREFIX}commands
+echo "/sbin/ip6tables -D INPUT -p tcp -i bat0 --destination-port 80 -j determines-autoupdates || echo 'Fail delete of reference ignored'" >> ${PREFIX}commands 
+echo "ip6tables -F determines-autoupdates && ip6tables -X determines-autoupdates || echo 'Fail delete of prior chain ignored.'" >> ${PREFIX}commands
+echo "ip6tables -N determines-autoupdates" >> ${PREFIX}commands
+cat ${PREFIX}04_permitted | while read macaddress
 do
-	echo /sbin/ip6tables -A INPUT -p tcp -i bat0 --destination-port 80 -m mac --mac-source $macaddress -j ACCEPT
-done > ${PREFIX}commands
-echo "/sbin/ip6tables -A INPUT -p tcp -i bat0 --destination-port 80 -j REJECT" >> ${PREFIX}commands
+	echo /sbin/ip6tables -A determines-autoupdates -i bat0 -m mac --mac-source $macaddress -j ACCEPT
+done >> ${PREFIX}commands
+echo "/sbin/ip6tables -A determines-autoupdates -p tcp -i bat0 --destination-port 80 -j REJECT" >> ${PREFIX}commands
+echo "/sbin/ip6tables -A INPUT -p tcp -i bat0 --destination-port 80 -j determines-autoupdates " >> ${PREFIX}commands
+chmod +x ${PREFIX}commands
 
-echo "First 5 lines:"
+echo "First and last 5 lines:"
 head -n 5 ${PREFIX}commands
-echo "Find all the commands to execute in '${PREFIX}commands'".
+echo "..."
+tail -n 5 ${PREFIX}commands
+echo "Find all the commands to execute in '${PREFIX}commands'"
